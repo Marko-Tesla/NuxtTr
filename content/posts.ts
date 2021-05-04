e872d07cbbc78a68b565a8c9c8bba72f2327ee03
Marat <marat@pingocean.com>
2021-05-04 15:56:44 +0600

    firstWorkingVersion

---
+++ content/posts.ts
@@ -0,0 +1,55 @@
+import { Post } from './Post'  
+
+export default [  
+  {
+    id: 1,  
+    title: 'Post 1',  
+    desc:  
+      'A short description of the post to keep the user interested.' +  
+      ' Description can be of different lengths, blocks are aligned' +  
+      ' to the height of the block with the longest description',  
+    file: 'content/posts/1.md',
+    img: 'assets/images/dashboard.svg',
+  },  
+  {
+    id: 2,  
+    title: 'Post 2',  
+    desc:  
+      'A short description of the post to keep the user interested.' +  
+      ' Description can be of different lengths, blocks are aligned' +  
+      ' to the height of the block with the longest description',  
+    file: 'content/posts/2.md',
+    img: 'assets/images/experts.svg',
+  },
+  {
+    id: 3,  
+    title: 'Post 3',  
+    desc:  
+      'A short description of the post to keep the user interested.' +  
+      ' Description can be of different lengths, blocks are aligned' +  
+      ' to the height of the block with the longest description',  
+    file: 'content/posts/3.md',
+    img: 'assets/images/mobileApp.svg',
+  },
+  {
+    id: 4,  
+    title: 'Post 4',  
+    desc:  
+      'A short description of the post to keep the user interested.' +  
+      ' Description can be of different lengths, blocks are aligned' +  
+      ' to the height of the block with the longest description',  
+    file: 'content/posts/4.md',
+    img: 'assets/images/settings.svg',
+  },
+
+  {  
+    id: 5,  
+    title: 'Post 5',  
+    desc:  
+      'A short description of the post to keep the user interested.' +  
+      ' Description can be of different lengths, blocks are aligned' +  
+      ' to the height of the block with the longest description',  
+    file: 'content/posts/5.md',
+    img: 'assets/images/socialGrowth.svg',
+  },  
+] as Post[]
--- nuxt.config.js
+++ nuxt.config.js
@@ -1,47 +1,49 @@
 export default {
-  // Target: https://go.nuxtjs.dev/config-target
+  // Target (https://go.nuxtjs.dev/config-target)
   target: 'static',
 
-  // Global page headers: https://go.nuxtjs.dev/config-head
+  // Global page headers (https://go.nuxtjs.dev/config-head)
   head: {
-    title: 'nuxt-tr',
-    htmlAttrs: {
-      lang: 'en'
-    },
+    title: 'nuxt-blog',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' }
+      { hid: 'description', name: 'description', content: '' },
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-    ]
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      {
+        as: 'style',
+        rel: 'stylesheet preload prefetch',
+        href: '/fonts/fonts.css',
+      },
+    ],
   },
 
-  // Global CSS: https://go.nuxtjs.dev/config-css
-  css: [
-  ],
+  // Global CSS (https://go.nuxtjs.dev/config-css)
+  css: ['~assets/styles/global', 'reset-css/reset.css'],
 
-  // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: [
-  ],
+  styleResources: {
+    scss: ['./assets/styles/variables.scss'],
+  },
 
-  // Auto import components: https://go.nuxtjs.dev/config-components
+  // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
+  plugins: ['~plugins/mixins/getDynamicFile.ts'],
+
+  // Auto import components (https://go.nuxtjs.dev/config-components)
   components: true,
 
-  // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
+  // Modules for dev and build (recommended) (https://go.nuxtjs.dev/config-modules)
   buildModules: [
     // https://go.nuxtjs.dev/typescript
     '@nuxt/typescript-build',
+    // https://go.nuxtjs.dev/stylelint
+    '@nuxtjs/stylelint-module',
   ],
 
-  // Modules: https://go.nuxtjs.dev/config-modules
-  modules: [
-    // https://go.nuxtjs.dev/bootstrap
-    'bootstrap-vue/nuxt',
-  ],
+  // Modules (https://go.nuxtjs.dev/config-modules)
+  modules: ['@nuxtjs/style-resources', '@nuxtjs/markdownit'],
 
-  // Build Configuration: https://go.nuxtjs.dev/config-build
-  build: {
-  }
+  // Build Configuration (https://go.nuxtjs.dev/config-build)
+  build: {},
 }
